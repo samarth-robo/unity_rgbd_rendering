84a4830bc0f5242b85eb3f0b722ce4917ea19eb2
Samarth Brahmbhatt <samarth.robo@gmail.com>
2021-12-29 00:00:54 -0800

    first commit

---
+++ Assets/RGBDRenderingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RGBDRenderingScript : MonoBehaviour
+{
+    [SerializeField]
+    Material material;
+    [SerializeField]
+    Camera cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       cam.depthTextureMode = cam.depthTextureMode | DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, this.material);
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# [RGB-D Rendering with Unity](https://samarth-robo.github.io/blog/2021/12/28/unity_rgbd_rendering.html)
+
+**Read more**: https://samarth-robo.github.io/blog/2021/12/28/unity_rgbd_rendering.html.
+
+<figure>
+    <img src="readme_images/scene.png" alt="scene" width="400">
+    <figcaption>scene</figcaption>
+</figure>
+
+<figure>
+    <img src="readme_images/depth.png" alt="scene" width="200">
+    <figcaption>depth</figcaption>
+</figure>
+<figure>
+    <img src="readme_images/rgb.png" alt="scene" width="200">
+    <figcaption>RGB</figcaption>
+</figure>
